acae24bba4b4f154907877ef0bc25c4dde852c33
joanajaupi <joanajaupi01@gmail.com>
2023-01-31 15:58:30 +0100

    dashboard

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -4,11 +4,6 @@
         public function index()
         {
             
-            $user = $this->load_model("userInfo");
-            $user_data = $user->check_login();
-            if(!is_bool($user_data)){
-                $data['user_data'] = $user_data;
-            }
             $data['page_title'] = "Dashboard";
             $this->view("admin", $data);
 
