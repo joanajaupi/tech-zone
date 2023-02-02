827afe5b575098222921486e853befd3cf6757c5
Kevin Tenolli <tenollikevin@gmail.com>
2023-02-02 01:14:32 +0100

    added some database queries for allproducts

---
+++ app/models/products.class.php
@@ -0,0 +1,27 @@
+"SELECT * FROM product;";
+
+
+
+<?php
+class Products
+{
+    public function getAllProducts()
+    {
+
+
+        $db = Database::getInstance();
+        $query = "SELECT * FROM product;";
+        $result = $db->read($query);
+        var_dump($result);
+        return $result;
+    }
+
+    public function getAllProductsByCategory()
+    {
+        $db = Database::getInstance();
+        $query = "SELECT * FROM product WHERE categoryID = placeholder;";
+        $result = $db->read($query);
+        var_dump($result);
+        return $result;
+    }
+}
