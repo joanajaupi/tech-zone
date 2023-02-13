38cfab867b8af13d544a6c2316f3269d4e250a9f
Kevin Tenolli <tenollikevin@gmail.com>
2023-02-14 00:43:56 +0100

    feat: Fully implement product category pages

--- public/assets/js/allproducts.js
+++ public/assets/js/allproducts.js
@@ -1,73 +1,94 @@
-document.addEventListener("DOMContentLoaded", function(){
-    get_products();
+let category;
+document.addEventListener("DOMContentLoaded", function () {
+  category = getCurrentCategory();
+  if (category === "allproducts") {
+    getAllProducts();
+  } else {
+    getByCategory(category);
+  }
 });
 
-function get_products(){
-    fetch("http://localhost/tech-zone/public/allproducts/fetchAll", {
-        method:"GET",
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(function(response){
-        return response.json();
-    }
-    ).then(function(data){
-        console.log(data);
-        populate(data);
-    }
-    ).catch(function(error){
-        console.log(error);
-    }
-    );
+function getAllProducts() {
+  fetch("http://localhost/tech-zone/public/allproducts/fetchAll", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      populate(data);
+    })
+    .catch(function (error) {
+      let products = document.querySelector("#products");
+      products.innerHTML = `<h1 class="text-center">An error occurred</h1>`;
+    });
 }
-function getByCategory(){
-    var category = document.querySelector("#category").value;
-    fetch("http://localhost/tech-zone/public/allproducts/fetchByCategory/"+category, {
-        method:"GET",
-        headers:{
-            "Content-Type":"application/json"
-        }
-    }).then(function(response){
-        return response.json();
+
+function getByCategory(category) {
+  fetch(
+    `http://localhost/tech-zone/public/allproducts/fetchByCategory/${category}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify({
+      //   category,
+      // }),
     }
-    ).then(function(data){
-        console.log(data);
-        populate(data);
-    }
-    ).catch(function(error){
-        console.log(error);
-    }
-    );
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      populate(data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      let products = document.querySelector("#products");
+      products.innerHTML = `<h1 class="text-center">An error occurred</h1>`;
+    });
 }
-const  outerDiv = document.querySelector("#products");
-var html = "";
-function populate(data){
-    var data = data['products']
-    console.log(data);
-    for(i = 0; i < data.length; i++){
-        html += `<div class="col-md-4"><div class="card mb-4 shadow-sm">
-            <img src="http://localhost/tech-zone/public/assets/images/${data[i].productImage}" class="card-img-top" alt="...">
+
+function populate(data) {
+  let products = document.querySelector("#products");
+  let html = "";
+  if (data.products.length > 0) {
+    for (let product of data.products) {
+      html += `<div class="col-md-4"><div class="card mb-4 shadow-sm">
+            <img src="http://localhost/tech-zone/public/assets/images/${product.productImage}" class="card-img-top" alt="...">
             <div class="card-body">
-                <p class="card-text" id="name">${data[i].productName}</p>
-                <p class="card-text" id="desc">${data[i].productDescription}</p>
-                <p class="card-text">${data[i].productPrice}$</p>
+                <p class="card-text" id="name">${product.productName}</p>
+                <p class="card-text" id="desc">${product.productDescription}</p>
+                <p class="card-text">${product.productPrice}$</p>
 
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
-                        <a href="http://localhost/tech-zone/public/product/${data[i].productID}" class="btn btn-sm btn-outline-secondary">View</a> 
+                        <a href="http://localhost/tech-zone/public/product/${product.productID}" class="btn btn-sm btn-outline-secondary">View</a>
                     </div>`;
-                    if(data[i].productQuantity > 0){
-                        html += `<small class="text-success">In Stock</small>`;
-                    }else{
-                        html += `<small class="text-danger">Out of Stock</small>`;
-                    }
-                    html += `
+      if (product.productQuantity > 0) {
+        html += `<small class="text-success">In Stock</small>`;
+      } else {
+        html += `<small class="text-danger">Out of Stock</small>`;
+      }
+      html += `
                 </div>
             </div>
         </div>
     </div>`;
     }
-    
+
     html += `</div>`;
-    outerDiv.innerHTML = html;
+    products.innerHTML = html;
+  } else {
+    products.innerHTML = `<h1 class="text-center">No products found</h1>`;
+  }
+}
+
+function getCurrentCategory() {
+  let category = window.location.href.split("/");
+  return category[category.length - 1];
 }
