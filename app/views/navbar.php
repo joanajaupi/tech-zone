02fe879ee04c672cff6f560987f354680424f4b5
joanajaupi <joanajaupi01@gmail.com>
2023-02-14 15:04:50 +0100

    fix:small fix

--- app/views/navbar.php
+++ app/views/navbar.php
@@ -1,13 +1,13 @@
 <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" style="color: #fff">Tech Zone</a>
+        <a href="<?=ROOT?>home" class="navbar-brand" style="color: #fff">Tech Zone</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="home">Home</a>
+                    <a class="nav-link active" aria-current="page" href="<?=ROOT?>home">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="<?= ROOT ?>allproducts">All Products</a>
