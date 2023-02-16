ca3df4ddbc8b74fe071f010a6972d7d6e1b174d6
Kevin Tenolli <tenollikevin@gmail.com>
2023-02-16 14:13:00 +0100

    fix: Fix review logic

--- app/controllers/reviews.php
+++ app/controllers/reviews.php
@@ -1,36 +1,37 @@
-<?php 
-    class reviews extends Controller{
+<?php
+class reviews extends Controller
+{
 
-        public function fetchReviews($productID)
-        {
-            $review = $this->load_model("review");
-            $data = $review->fetchReviews($productID);
-            if(isset($_SESSION['userID'])){
-                $arr['userID'] = $_SESSION['userID'];
-            }
-            if($data == false)
-            {
-                $arr['message'] = "No reviews found";
-                $arr['message_type'] = "error";
-                $arr['data'] = "";
-                echo json_encode($arr);
-                return;
-            }
-            else{
-                $arr['message'] = "Reviews fetched successfully";
-                $arr['message_type'] = "success";
-                $arr['data'] = $data;
-                echo json_encode($arr);
-            }
+    public function fetchReviews($productID)
+    {
+        $review = $this->load_model("review");
+        $data = $review->fetchReviews($productID);
+        if (isset($_SESSION['userID'])) {
+            $arr['userID'] = $_SESSION['userID'];
         }
-        public function publishReview()
-        {
-            //check if user is logged in
-            if(isset($_SESSION['userID'])){
-                $data = file_get_contents("php://input");
-                $data = json_decode($data);
-                if (is_object($data)) {
-                    $review = $this->load_model("review");
+        if ($data == false) {
+            $arr['message'] = "No reviews found";
+            $arr['message_type'] = "error";
+            $arr['data'] = "";
+            echo json_encode($arr);
+            return;
+        } else {
+            $arr['message'] = "Reviews fetched successfully";
+            $arr['message_type'] = "success";
+            $arr['data'] = $data;
+            echo json_encode($arr);
+        }
+    }
+    public function publishReview()
+    {
+        //check if user is logged in
+        if (isset($_SESSION['userID'])) {
+            $data = file_get_contents("php://input");
+            $data = json_decode($data);
+            if (is_object($data)) {
+                $review = $this->load_model("review");
+                $hasWrittenReview = $review->hasWrittenReview($data->productID, $_SESSION['userID']);
+                if ($hasWrittenReview === false) {
                     $check = $review->createReview($data->productID, $_SESSION['userID'], $data->review, $data->rate);
                     if ($check) {
                         $arr['message'] = "Review published successfully";
@@ -43,13 +44,18 @@
                         $arr['data'] = "";
                         echo json_encode($arr);
                     }
-                }
-                else{
-                    $arr['message'] = "Review could not be published";
-                    $arr['message_type'] = "error";
+                } else {
+                    $arr['message'] = "You have already written a review for this product";
+                    $arr['message_type'] = "duplicate";
                     $arr['data'] = "";
                     echo json_encode($arr);
                 }
-             }
+            } else {
+                $arr['message'] = "Review could not be published";
+                $arr['message_type'] = "error";
+                $arr['data'] = "";
+                echo json_encode($arr);
+            }
         }
     }
+}
--- app/models/review.class.php
+++ app/models/review.class.php
@@ -1,27 +1,43 @@
-<?php 
+<?php
 
-    class review{
+class review
+{
 
-        public function fetchReviews($productID){
-            $db = Database::getInstance();
-            $query = "SELECT u.name, r.review, r.publishedAt, r.rate
+    public function fetchReviews($productID)
+    {
+        $db = Database::getInstance();
+        $query = "SELECT u.name, r.review, r.publishedAt, r.rate
                         FROM reviews r JOIN userInfo u ON r.userID = u.userID WHERE r.productID = :productID";
-            $arr['productID'] = $productID;
-            $result = $db->read($query, $arr);
-            return $result;
-        }
-
-        public function createReview($productID, $userID, $review, $rate){
-            $db = Database::getInstance();
-            $date = date("Y-m-d H:i:s");
-            $query = "INSERT INTO reviews (productID, userID, review, rate, publishedAt) VALUES (:productID, :userID, :review, :rate, :publishedAt)";           
-            $arr['productID'] = $productID;
-            $arr['userID'] = $userID;
-            $arr['review'] = $review;
-            $arr['rate'] = $rate;
-            $arr['publishedAt'] = $date;
-            $result = $db->write($query, $arr);
-            return $result;
-        }
-
+        $arr['productID'] = $productID;
+        $result = $db->read($query, $arr);
+        return $result;
     }
+
+    public function createReview($productID, $userID, $review, $rate)
+    {
+        $db = Database::getInstance();
+        $date = date("Y-m-d H:i:s");
+        $query = "INSERT INTO reviews (productID, userID, review, rate, publishedAt) VALUES (:productID, :userID, :review, :rate, :publishedAt)";
+        $arr['productID'] = $productID;
+        $arr['userID'] = $userID;
+        $arr['review'] = $review;
+        $arr['rate'] = $rate;
+        $arr['publishedAt'] = $date;
+        $result = $db->write($query, $arr);
+        return $result;
+    }
+
+    public function hasWrittenReview($productID, $userID)
+    {
+        $db = Database::getInstance();
+        $query = "SELECT * FROM reviews WHERE productID = :productID AND userID = :userID";
+        $arr['productID'] = $productID;
+        $arr['userID'] = $userID;
+        $result = $db->read($query, $arr);
+        if ($result) {
+            return  true;
+        } else {
+            return false;
+        }
+    }
+}
