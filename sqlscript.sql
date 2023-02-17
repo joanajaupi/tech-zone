58a69bcc79f55188d989772c32d0668a2ccc1e68
joanajaupi <joanajaupi01@gmail.com>
2023-02-17 12:05:21 +0100

    sql dumps and images

--- sqlscript.sql
+++ sqlscript.sql
@@ -60,3 +60,19 @@ ALTER TABLE `reviews`
 
 ALTER TABLE `reviews`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `category` (`categoryID`, `categoryName`) VALUES
+(5, 'Accessories'),
+(1, 'Laptop'),
+(4, 'Pc'),
+(38, 'Phone'),
+(7, 'Screen'),
+(2, 'Tv');
+COMMIT;
+
+INSERT INTO `product` (`productID`, `productName`, `productDescription`, `productPrice`, `productQuantity`, `productCategoryID`, `productImage`) VALUES
+(68, 'Apple iPhone 14 Pro,128GB', 'First-class design, durability, functions and technology.', 1500, 10, 38, 'iphone.jpg'),
+(69, 'Dell Latitude 5520', 'Laptop Dell Latitude 5520 (S002L552015W11PL), 15.6\" Full HD, Intel Core i5, 8GB RAM DDR4, 256GB SSD, Intel Iris Xe Graphics, i hirtë', 1074, 5, 1, 'dell.jpg'),
+(70, 'Assus Rog', 'Laptop ASUS ROG Strix SCAR 18 (2023) G834, 18\", Intel Core i9, 32GB RAM, 2x 1TB SSD, NVIDIA GeForce RTX 4090, i zi', 4000, 5, 1, 'assusrog.jpg'),
+(71, 'Laptop Lenovo NB Legion ', 'Laptop Lenovo NB Legion 5 15IAH7H, 15.6\", Intel Core I7-12700H, 2x 16GB RAM, 2x 1TB SSD, NVIDIA GeForce RTX 3070 8GBG DDR6', 2000, 10, 1, 'legion.jpg');
+COMMIT;
